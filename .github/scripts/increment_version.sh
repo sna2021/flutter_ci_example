70e29639ed760f0352ab7a26ff9a3394915628ab
nikita <sna2021@narod.ru>
2022-01-27 15:49:16 +0300

    fix increment_version.sh

--- .github/scripts/increment_version.sh
+++ .github/scripts/increment_version.sh
@@ -3,7 +3,7 @@ set -e
 # Find and increment the version number.
 perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' ./pubspec.yaml
 
-# Commit and tag this change.
-version=`grep 'version: ' ./pubspec.yaml | sed 's/version: //'`
-git commit -m "version $version" ./pubspec.yaml
-git tag $version
+## Commit and tag this change.
+#version=`grep 'version: ' ./pubspec.yaml | sed 's/version: //'`
+#git commit -m "version $version" ./pubspec.yaml
+#git tag $version
