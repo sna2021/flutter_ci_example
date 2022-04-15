eed27032bea7696308ef0ab40c8fe8910964cecb
nikita <sna2021@narod.ru>
2022-04-15 13:02:41 +0300

    upd main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:app_logger/app_logger.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  ///sdfsdf
   final env = AppEnv.initFromConsole();
   runApp(MaterialApp(
     title: 'Env test',
