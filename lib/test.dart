c955a734adf0592a9aa68ed157326ebf5460e5d4
nikita <sna2021@narod.ru>
2022-04-15 11:28:35 +0300

    add BIG_CLASS

---
+++ lib/test.dart
@@ -0,0 +1,26 @@
+class BigClass {
+  final String value1;
+  final String value2;
+  final String value3;
+  final String value4;
+  final String value5;
+  final String value6;
+  final String value7;
+  final String value8;
+  final String value9;
+  final String value10;
+  final String value11;
+
+  BigClass(
+      this.value1,
+      this.value2,
+      this.value3,
+      this.value4,
+      this.value5,
+      this.value6,
+      this.value7,
+      this.value8,
+      this.value9,
+      this.value10,
+      this.value11);
+}
